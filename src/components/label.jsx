c2487e7be2c23fd765222de1921d13a8ba69c97d
Bastiaan Niamut <bastiaan.nia@gmail.com>
2018-03-28 12:38:22 +0200

    resolves #4 - more space between label and input on mobile

--- src/components/label.jsx
+++ src/components/label.jsx
@@ -1,41 +1,48 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import {miniMargin, halfMargin, colors} from '../theme.js';
+import { miniMargin, halfMargin, colors } from '../theme.js';
 import { InfoIcon } from './infoIcon';
 
 const LabelComponent = styled.h3`
-    font-size: 18px;
-    font-weight: 600;
-    color: ${colors.darkGrey};
-    min-height: 30px;
-    padding-top: 6px;
+  font-size: 18px;
+  font-weight: 600;
+  color: ${colors.darkGrey};
+  min-height: 30px;
+  padding-top: 6px;
+
+  @media (min-width: 10px) and (max-width: 800px) {
+    padding-bottom: 20px;
+  }
 `;
 
 const Star = styled.span`
-    color: ${colors.green};
-`
+  color: ${colors.green};
+`;
 
 export class Label extends Component {
   static propTypes = {
     labelText: PropTypes.string,
     star: PropTypes.bool,
     popupText: PropTypes.string
-  }
+  };
 
   static defaultProps = {
-    star: true,
-    }   
+    star: true
+  };
 
   render() {
-    const {labelText, star, popupText} = this.props;
+    const { labelText, star, popupText } = this.props;
 
     return (
       <LabelComponent>
-        {labelText}{star ? <Star>*</Star> : null}&nbsp;{popupText ? <InfoIcon popupText={popupText} /> : null }
+        {labelText}
+        {star ? <Star>*</Star> : null}&nbsp;{popupText ? (
+          <InfoIcon popupText={popupText} />
+        ) : null}
       </LabelComponent>
-    )
+    );
   }
 }
 
-export default Label
+export default Label;
